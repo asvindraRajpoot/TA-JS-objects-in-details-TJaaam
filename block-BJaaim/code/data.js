9bcb7b5b13319ea8a498c7dcb0052f44f854f3cf
asvindra <aswin1399904@gmail.com>
2021-10-07 16:18:46 +0530

    Completed Quiz App

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,38 @@
+
+const quizCollection=[
+    {
+        title:'What is first Alphabet?',
+        options:[
+            'A',
+            'B',
+            'C',
+            'D'
+        ],
+        correctAnsIndex:0,
+
+    },
+    {
+        title:'What is Capital of india?',
+        options:[
+            'Delhi',
+            'Mumbai',
+            'Kolkata',
+            'Agra'
+        ],
+        correctAnsIndex:0,
+
+    },
+    {
+        title:'Who is father of India?',
+        options:[
+           
+            'Sn Bosh',
+            'Mahatma Gandhi',
+            'Patil',
+            'Mohan'
+        ],
+        correctAnsIndex:1,
+
+    },
+
+]
--- block-BJaaim/code/index.js
+++ block-BJaaim/code/index.js
@@ -43,51 +43,96 @@
 //   This will create the ui of the whole app.
 // - updateScore
 //   This method will update the score.
+
 let root=document.querySelector('.box');
-
-
+let next=document.querySelector('.next');
+let prev=document.querySelector('.prev');
+let res=document.querySelector('.res-btn');
+let span=document.querySelector('span');
 
 class Question{
-    constructor(title,options,correctAns){
+    constructor(title,options,correctAnsIndex){
         this.title=title;
         this.options=options;
-        this.correctAns=correctAns;
+        this.correctAnsIndex=correctAnsIndex;
     }
   
     isCorrect(answer){
-        return answer===this.correctAns;
+        console.log(this.options[this.correctAnsIndex])
+        return answer===this.options[this.correctAnsIndex];
     }
     getCorrectAnswer(){
-        return this.correctAns;
+        return this.options[this.correctAnsIndex];
     }
-    clickHandler(event){
-        let score=0;
-        let state= event.target.checked;
-        let value= event.target.nextElementSibling.innerText;
-          
-        console.log(value,getCorrectAnswer());
-        if(state && value === question1.getCorrectAnswer()){
-            score++;
-            span.innerText=`Score:${score}`;
-            console.log('ans is correct');
-        }else{
-            console.log('Answer is not correct');
-        }
-    
-        }
+   
+     
+}
 
+class Quiz{
+    constructor(Questions=[],score=0){
+        this.Questions=Questions;
+        this.score=score;
+        this.activeIndex=0;
+    }
+    incrementScore(){
+        this.score=this.score+1;
+    }
+
+
+    nextQuestion(){
+        this.activeIndex=this.activeIndex+1
+        this.createUI();    
+    }
+    prevQuestion(){
+        this.activeIndex=this.activeIndex-1
+        this.createUI();    
+    }
+    addQuestion(title,options,correctAnsIndex){
+        let question=new Question(title,options,correctAnsIndex);
+        this.Questions.push(question);
+        console.log(this.Questions);
+    }
+    handleButton(){
+     
+        console.log(this.activeIndex,this.Questions.length);
+       if(this.activeIndex===0){
+           prev.style.display='none'
+           next.style.display='flex'
+           res.style.display="none"
+       }
+       else if(this.activeIndex===this.Questions.length-1){
+              next.style.display='none'
+              prev.style.display='flex'
+              res.style.display="flex"
+        }
+        else{
+            next.style.display='flex'
+            prev.style.display='flex'
+        }
+       
+
+
+    }
+    updateScore(){
+        let score=0;
+        //
+        this.score=score+1;
+        return this.score;
+    }
     createUI(){
-        console.log('it is one UI')
+        root.innerHTML="";
+        console.log(this.activeIndex,this.Questions[0]);
+        let activeQuestion=this.Questions[this.activeIndex];
+        console.log(activeQuestion.title);
         let qBox=document.createElement('div');
-        qBox.innerHTML="";
         qBox.classList.add('qbox');
         let h2=document.createElement('h2');
-        h2.innerText=this.title;
+        h2.innerText=activeQuestion.title;
         let ul=document.createElement('ul');
         let op1=document.createElement('div');
         op1.classList.add('q');
         let li1=document.createElement('li');
-        li1.innerText=this.options[0];
+        li1.innerText=activeQuestion.options[0];
         let check1=document.createElement('input');
         check1.setAttribute('type','checkbox');
         op1.append(check1,li1);
@@ -95,7 +140,7 @@ class Question{
         let op2=document.createElement('div');
         op2.classList.add('q');
         let li2=document.createElement('li');
-        li2.innerText=this.options[1];
+        li2.innerText=activeQuestion.options[1];
         let check2=document.createElement('input');
         check2.setAttribute('type','checkbox');
         op2.append(check2,li2);
@@ -103,7 +148,7 @@ class Question{
         let op3=document.createElement('div');
         op3.classList.add('q');
         let li3=document.createElement('li');
-        li3.innerText=this.options[2];
+        li3.innerText=activeQuestion.options[2];
         let check3=document.createElement('input');
         check3.setAttribute('type','checkbox');
         op3.append(check3,li3);
@@ -111,7 +156,7 @@ class Question{
         let op4=document.createElement('div');
         op4.classList.add('q');
         let li4=document.createElement('li');
-        li4.innerText=this.options[3];
+        li4.innerText=activeQuestion.options[3];
         let check4=document.createElement('input');
         check4.setAttribute('type','checkbox');
         op4.append(check4,li4);
@@ -119,61 +164,41 @@ class Question{
         ul.append(op1,op2,op3,op4);
         qBox.append(h2,ul);
         root.append(qBox);
+       
         let ans=document.querySelectorAll('input');
-        let span=document.querySelector('span');
-        ans.forEach(checkbox=>{
-            checkbox.addEventListener('click',this.clickHandler);
-        })
-    }
-            
-}
-
-class Quiz{
-    constructor(allQuestions=[],activeIndex=0,score=0){
-        this.allQuestions=allQuestions;
-        this.activeIndex=activeIndex;
-        this.score=score;
-    }
-    nextQuestion(){
-
-      (this.allQuestions[this.activeIndex+1]).createUI();
-    }
-
-    createUI(){
       
-        console.log('its whole UI')
-        let question1=new Question('What is capital of India?',['Delhi','Mumbai','Kolkata','Banglore'],'Delhi');
-        let question2=new Question('What is capital of UP?',['Delhi','Mumbai','Kolkata','Lucknow'],'Lucknow');
-        this.allQuestions.push(question1);
-        this.allQuestions.push(question2);
-        this.allQuestions.forEach(ques=>{
+        this.handleButton();
+        ans.forEach(checkbox=>{
+            checkbox.addEventListener('click',(event=>{
+                console.log(activeQuestion.getCorrectAnswer(),event.target.nextElementSibling.innerText );
 
-            
-            ques.createUI();
+                if(checkbox.checked && activeQuestion.options[activeQuestion.correctAnsIndex]===event.target.nextElementSibling.innerText){
+                      this.updateScore();
+                }else{
+                    console.log('ans is not correct');
+                }
+            }))
         })
-        let next=document.createElement('button');
-         next.innerText='Next';
-        
-         root.append(next);
-
-
+     
     }
-    updateScore(){
-      let updatedScore=0;
-    
-    }
+
+          
 }
 
 let quiz=new Quiz()
-quiz.createUI();
-let nextBtn=document.querySelector('button');
-function nextHandler(){
 
-    root.innerHTML=""
-    quiz.createUI();
-    quiz.nextQuestion();
-}
-nextBtn.addEventListener('click',nextHandler);
+quizCollection.forEach((question)=>{
+    quiz.addQuestion(
+        question.title,question.options,question.correctAnsIndex
+    )
+})
+
+quiz.createUI();
+
+next.addEventListener('click',quiz.nextQuestion.bind(quiz))
+prev.addEventListener('click',quiz.prevQuestion.bind(quiz))
+res.addEventListener('click',(event)=>{
+    span.innerText=`Score:${quiz.score}`});
 
 
 
